6cdb97954dc2271727adbe0a39fa5389e1abf962
Ernande Linhares <ernandeinf@gmail.com>
2017-12-06 16:54:23 -0200

    Ajustes heroku

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 "use strict";
 
-const io = require('socket.io')();
+const PORT = process.env.PORT || 5000;
+
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+const path = require("path");
 
 const getRandomInt = require('./modules/getRandomInt');
 
-const PORT = process.env.PORT || 5000;
+
+app.disable("x-powered-by");
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req,res) => {
+	res.sendFile(path.join(__dirname+'/index.html'));
+});
+
 
 io.on('connection', function(socket){
 
@@ -23,4 +39,4 @@ io.on('connection', function(socket){
 });
 
 
-io.listen(PORT);
+server.listen(PORT);
